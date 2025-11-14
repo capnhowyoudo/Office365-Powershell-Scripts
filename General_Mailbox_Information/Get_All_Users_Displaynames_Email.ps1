6b6da4fc96910b7541ea5cacfabcfab36cb8b60c
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-14 16:43:47 -0600

    Update Get_All_Users_Displaynames_Email.ps1

--- General_Mailbox_Information/Get_All_Users_Displaynames_Email.ps1
+++ General_Mailbox_Information/Get_All_Users_Displaynames_Email.ps1
@@ -4,3 +4,6 @@ Retrieves the display names and email addresses of all users.
 #>
 
 Get-Mailbox | Select-Object DisplayName,PrimarySmtpAddress
+
+#export to csv
+Get-Mailbox | Select-Object DisplayName,PrimarySmtpAddress | Export-Csv -Path "C:\Temp\Mailboxes.csv" -NoTypeInformation -Encoding UTF8
