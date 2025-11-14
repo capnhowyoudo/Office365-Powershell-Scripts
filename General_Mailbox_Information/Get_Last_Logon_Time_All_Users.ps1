d5b764e567b99571c01c25b049b6f0674c872144
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-14 16:42:50 -0600

    Create Get_Last_Logon_Time_All_Users.ps1
    Displays the last logon time for all users.

---
+++ General_Mailbox_Information/Get_Last_Logon_Time_All_Users.ps1
@@ -0,0 +1,9 @@
+<#
+.SYNOPSIS
+Displays the last logon time for all users.
+#>
+
+Get-Mailbox | Get-MailboxStatistics | Where-Object {$_.LastLogonTime} | Select-Object DisplayName,Name,LastLogonTime 
+
+#Export to CSV
+Get-Mailbox | Get-MailboxStatistics | Where-Object {$_.LastLogonTime} | Select-Object DisplayName,Name,LastLogonTime | Export-Csv -Path "C:\Temp\MailboxLastLogon.csv" -NoTypeInformation -Encoding UTF8
