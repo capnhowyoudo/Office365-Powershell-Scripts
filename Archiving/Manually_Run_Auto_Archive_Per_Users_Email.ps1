380ea781d5922450444bb06ba97c5acb3f2f8a71
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-17 11:10:28 -0600

    Create Manually_Run_Auto_Archive_Per_Users_Email.ps1
    Starts the Managed Folder Assistant for a specific mailbox to process retention policies and archive rules

---
+++ Archiving/Manually_Run_Auto_Archive_Per_Users_Email.ps1
@@ -0,0 +1,6 @@
+<#
+.SYNOPSIS
+Starts the Managed Folder Assistant for a specific mailbox to process retention policies and archive rules.
+#>
+
+Start-ManagedFolderAssistant -Identity user@example.com
