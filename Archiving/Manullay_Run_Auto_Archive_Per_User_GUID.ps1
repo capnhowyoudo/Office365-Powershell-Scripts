9585a2acb1313fff370d056a3315189be58f04d0
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-17 11:15:06 -0600

    Update Manullay_Run_Auto_Archive_Per_User_GUID.ps1

--- Archiving/Manullay_Run_Auto_Archive_Per_User_GUID.ps1
+++ Archiving/Manullay_Run_Auto_Archive_Per_User_GUID.ps1
@@ -3,7 +3,8 @@
 Retrieves the mailbox GUID and location type for a user mailbox, then starts the Managed Folder Assistant on the primary mailbox using the GUID.
 
 .NOTES
-The GUID used with Start-ManagedFolderAssistant should be the primary mailbox GUID retrieved from Get-MailboxLocation.
+The GUID used in Start-ManagedFolderAssistant should be the Primary Mailbox GUID retrieved from Get-MailboxLocation. 
+Replace the generic GUID with the actual primary mailbox GUID of the user.
 #>
 
 # Get mailbox GUID and location
@@ -11,4 +12,3 @@ Get-MailboxLocation -User user@example.com | FL MailboxGuid, MailboxLocationType
 
 # Start the Managed Folder Assistant on the primary mailbox using a generic GUID
 Start-ManagedFolderAssistant 00000000-0000-0000-0000-000000000000
-
