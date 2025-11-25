dd87e6b6f3c9ccb3d1bc78010b3796117d4059b8
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-25 17:00:08 -0600

    Add script to check password expiration status for user
    This script retrieves a Microsoft 365 user and checks if their password is set to never expire, using the Microsoft Graph PowerShell module.

---
+++ Passwords/Password_Expirations/Check_Password__Expiration_Status_Per_User.ps1
@@ -0,0 +1,13 @@
+<#
+.SYNOPSIS
+Retrieves a Microsoft 365 user and checks if their password is set to never expire.
+
+.DESCRIPTION
+Gets the specified user by UserId or UPN, selects the UserPrincipalName property, 
+and evaluates the PasswordPolicies property to indicate if the password never expires.
+
+.NOTES
+Requires Microsoft Graph PowerShell module (Mg) and appropriate permissions.
+#>
+
+Get-MgUser -UserId <user id or UPN> -Property UserPrincipalName, PasswordPolicies | Select-Object UserPrincipalName,@{N="PasswordNeverExpires";E={$_.PasswordPolicies -match "DisablePasswordExpiration"}}
