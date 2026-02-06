87fd9d814658b2163a1307a3179bfafc65e7dfb3
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2026-02-06 14:47:33 -0600

    Update Add_Licenses_In_Bulk.ps1

--- Licenses/Add_Licenses_In_Bulk.ps1
+++ Licenses/Add_Licenses_In_Bulk.ps1
@@ -22,7 +22,7 @@ user1@example.com
 user2@example.com
 user3@example.com
 
-#Connect to MS Graph Private Window (Some cmdlets may not work, known bug when using  -UseDeviceCode)
+Optional Connect to MS Graph Private Window (Some cmdlets may not work, known bug when using  -UseDeviceCode)
 Connect-Graph -Scopes User.ReadWrite.All, Organization.Read.All -UseDeviceCode
 #>
 
