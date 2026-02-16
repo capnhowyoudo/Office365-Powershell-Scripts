6c8b29fb26854c3af21bda91838a4060c3f858eb
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2026-02-16 15:38:25 -0600

    Update Identifies_Email_Address_Associations.ps1

--- General_Mailbox_Information/Identifies_Email_Address_Associations.ps1
+++ General_Mailbox_Information/Identifies_Email_Address_Associations.ps1
@@ -3,4 +3,10 @@
 Identifies whether an email address belongs to a user, group, distribution list, mailbox, or public folder.
 #>
 
+#cmdlet for Exchange online
+
 Get-EXORecipient -ResultSize unlimited | Where-Object {$_.EmailAddresses -match "user@example.com"} | fl Name, RecipientType, EmailAddresses
+
+#cmdlet for On prem Exchange
+
+Get-Recipient -ResultSize Unlimited | Where-Object {$_.EmailAddresses -match "user@example.com"} | Format-List Name, RecipientType, EmailAddresses
