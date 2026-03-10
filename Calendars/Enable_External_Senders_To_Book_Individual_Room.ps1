c26ccb1db8d6dbced629e5ffd613c7646de9263c
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2026-03-10 11:28:19 -0500

    Add command to list room mailbox email addresses
    Added a command to find all room mailbox email addresses.

--- Calendars/Enable_External_Senders_To_Book_Individual_Room.ps1
+++ Calendars/Enable_External_Senders_To_Book_Individual_Room.ps1
@@ -10,7 +10,8 @@ from outside the organization are handled according to the mailbox’s resource
 calendar processing rules.
 
 .NOTES
-Generic example for enabling external meeting request processing on a mailbox.
+To find all room mailbox email addresses, you can use:
+Get-Mailbox -Filter '(RecipientTypeDetails -eq "RoomMailbox")' -ResultSize Unlimited | Select-Object DisplayName, PrimarySmtpAddress
 #>
 
 # Enable processing of external meeting requests for a mailbox using a generic email
